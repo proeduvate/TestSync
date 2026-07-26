1f1201e9f88ba426f8b94038fc976b2f53015c3c
Kaul19090 <kaul190905@gmail.com>
2026-07-26 09:16:06 +0530

    signup update

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -382,7 +382,7 @@ function Signup() {
                     variant="outline"
                     size="lg"
                     fullWidth
-                    onClick={() => signupWithGoogle(formData.role)}
+                    onClick={() => loginWithGoogle(formData.role)}
                     className="google-btn"
                     icon={<img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google" width="18" height="18" />}
                 >
